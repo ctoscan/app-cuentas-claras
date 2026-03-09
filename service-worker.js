e168d3ad38c8efb2e14917b6ffc67fe062babe48
Kiki <123988577+ctoscan@users.noreply.github.com>
2026-03-09 16:14:26 -0300

    Upgrade service worker to version 4 with cache management
    Se modifico service-worker.js completo

--- service-worker.js
+++ service-worker.js
@@ -1,27 +1,69 @@
-const CACHE_NAME = "cuentas-claras-v3";
+const CACHE_NAME = "cuentas-claras-v4";
 
 const urlsToCache = [
   "./",
-  "index.html",
-  "styles.css",
-  "app.js",
-  "manifest.json",
-  "icons/icon-192.png",
-  "icons/icon-512.png"
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
 ];
 
 self.addEventListener("install", event => {
+
+  self.skipWaiting();
+
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
   );
+
+});
+
+self.addEventListener("activate", event => {
+
+  event.waitUntil(
+
+    caches.keys().then(cacheNames => {
+
+      return Promise.all(
+
+        cacheNames.map(cache => {
+
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+
+        })
+
+      );
+
+    })
+
+  );
+
+  self.clients.claim();
+
 });
 
 self.addEventListener("fetch", event => {
+
   event.respondWith(
+
     caches.match(event.request)
-      .then(response => response || fetch(event.request))
+      .then(response => {
+
+        if (response) {
+          return response;
+        }
+
+        return fetch(event.request);
+
+      })
+
   );
+
 });
-
-
