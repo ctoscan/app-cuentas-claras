5a3fb9cf3fc27494e3e8a7fab98efb6090535158
Kiki <123988577+ctoscan@users.noreply.github.com>
2026-03-09 17:08:07 -0300

    Upgrade service worker cache to version 2
    Updated cache version and added new files to cache.

--- service-worker.js
+++ service-worker.js
@@ -1,35 +1,25 @@
-const CACHE_NAME = "cuentas-claras-v1"
+const CACHE_NAME = "cuentas-claras-v2";
 
 const urlsToCache = [
-
-"/",
-"/index.html",
-"/styles.css",
-"/app.js",
-"/charts.js"
-
-]
-
+  "./",
+  "index.html",
+  "styles.css",
+  "app.js",
+  "manifest.json",
+  "icons/icon-192.png",
+  "icons/icon-512.png"
+];
 
 self.addEventListener("install", event => {
-
-event.waitUntil(
-
-caches.open(CACHE_NAME)
-.then(cache => cache.addAll(urlsToCache))
-
-)
-
-})
-
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
 
 self.addEventListener("fetch", event => {
-
-event.respondWith(
-
-caches.match(event.request)
-.then(response => response || fetch(event.request))
-
-)
-
-})
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
