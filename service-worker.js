35fbf1be6201dc5b708535c134fd80dce52a61d3
Kiki <123988577+ctoscan@users.noreply.github.com>
2026-03-09 15:30:34 -0300

    Update service worker to cache additional files
    Se edito todo el service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,36 +1,25 @@
 const CACHE_NAME = "cuentas-claras-v1";
 
 const urlsToCache = [
+  "./",
   "index.html",
   "styles.css",
-  "app.js"
+  "app.js",
+  "manifest.json",
+  "icons/icon-192.png",
+  "icons/icon-512.png"
 ];
 
 self.addEventListener("install", event => {
-
   event.waitUntil(
-
     caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
-
+      .then(cache => cache.addAll(urlsToCache))
   );
-
 });
 
-
 self.addEventListener("fetch", event => {
-
   event.respondWith(
-
     caches.match(event.request)
-      .then(response => {
-
-        return response || fetch(event.request);
-
-      })
-
+      .then(response => response || fetch(event.request))
   );
-
 });
